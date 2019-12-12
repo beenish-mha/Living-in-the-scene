f57ea93e81f4c094d115a827c16af0d777a7c920
Bethan James <bethanjames@Bethans-MacBook-Pro.local>
2019-12-12 20:34:32 +0000

    adding to the styling

--- script.js
+++ script.js
@@ -1,26 +1,31 @@
 //"On Click Event" listener (Search-button n.b. may be called something else)
-function creatCardElement(data){
+function creatCardElement(data) {
   var $textElement = $("<li>").attr("class", "movie-title").text(data);
-    $(".list").append($textElement);
+  $(".list").append($textElement);
 }
 
-$("#search-button").on("click", function(){
+var $formElement = $(".control");
+
+function handleSubmit(event) {
+  event.preventDefault();
   var $movie = $("#movie-name").val();
   console.log($movie);
   var queryURL = "https://api.themoviedb.org/3/search/movie?api_key=0a2e111476bfd341e9cc4952d7f4e484&query=" + $movie;
 
 
-// AJAX function
-$.ajax({
-   url: queryURL,
+  // AJAX function
+  $.ajax({
+    url: queryURL,
     method: "GET"
-  }).then(function(response) {
+  }).then(function (response) {
 
-// calling function to create a list of movies
-    response.results.forEach(function(element){
-      creatCardElement(element.title)}
-      );
+    // calling function to create a list of movies
+    response.results.forEach(function (element) {
+      creatCardElement(element.title)
+    }
+    );
 
   });
+}
 
-});
+$formElement.on("submit", handleSubmit);
